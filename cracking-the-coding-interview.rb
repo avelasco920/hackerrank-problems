91be7bebda6fbb80c25ba2956fba4bcf6a3a9c57
Angel Velasco <avelasco920@gmail.com>
2018-02-07 10:28:24 -0800

    initial commit

---
+++ cracking-the-coding-interview.rb
@@ -0,0 +1,12 @@
+
+# ------------------------------
+# ------------------------------
+# Strings: Making Anagrams
+# ------------------------------
+# ------------------------------
+
+def strings_make_anograms(a, b)
+  a + b
+end
+
+p strings_make_anograms("cde", "abc")
