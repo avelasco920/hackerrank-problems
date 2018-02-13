fc858eecd1093c8d73029b86135a6081c109a395
Angel Velasco <avelasco920@gmail.com>
2018-02-12 17:32:37 -0800

    edits to file

--- cracking-the-coding-interview.rb
+++ cracking-the-coding-interview.rb
@@ -5,8 +5,22 @@
 # ------------------------------
 # ------------------------------
 
-def strings_make_anograms(a, b)
-  a + b
+def strings_make_anagrams(a, b)
+  find_anagram(a, b)
+  # count = 0
+  # a.downcase.split('').each { |char| count += 1 if b.downcase.split('').include?(char) }
+  # (a.length - count) + (b.length - count)
 end
 
-p strings_make_anograms("cde", "abc")
+def find_anagram(a, b)
+  a_count = 0
+  b_count = 0
+  word2 = b.dup.split('')
+  a.split('').each do |char|
+    if word2.include?(char)
+      
+    end
+  end
+end
+
+p strings_make_anagrams("cde", "abc")
